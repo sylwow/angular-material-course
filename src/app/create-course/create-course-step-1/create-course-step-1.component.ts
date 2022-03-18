08e9cb6cb4badbe9e697ca96e5cf37ea12558836
Sylwow <Sylwo1003@gmail.com>
2022-03-18 14:32:16 +0100

    finish first step and add mat tooltip and add basic table

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatCalendarCellClassFunction, MatCalendarCellCssClasses } from '@angular/material/datepicker';
 
 
 @Component({
   selector: "create-course-step-1",
-  templateUrl:"create-course-step-1.component.html",
+  templateUrl: "create-course-step-1.component.html",
   styleUrls: ["create-course-step-1.component.scss"]
 })
 export class CreateCourseStep1Component {
@@ -19,8 +20,16 @@ export class CreateCourseStep1Component {
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: ['asdasdasdasdasd asdasdasdasdasdasdasdasdasdasdasdasdasdasdasda sdasdasdasdasdasdasdasdasdasdasdasdasdasdasd sdasdasdasdasdasdasdasdasdasdasdasdasdasdasd sdasdasdasdasdasdasdasdasdasdasdasdasdasdasd', [Validators.required, Validators.minLength(3)]]
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate();
+    if (view == 'month') {
+      return date === 1 ? 'highlight-date' : '';
+    }
+    return '';
+  }
 
   constructor(private fb: FormBuilder) {
 
@@ -29,5 +38,6 @@ export class CreateCourseStep1Component {
   get courseTitle() {
     return this.form.controls['title'];
   }
+
 
 }
